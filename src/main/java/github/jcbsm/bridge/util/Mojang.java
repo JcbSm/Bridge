b795a7c164011cfaa212670da8e65d5f1c8c73af
JcbSm <49797894+JcbSm@users.noreply.github.com>
2023-06-22 00:57:27 +0100

    feat: Mojang UUID fetch util

---
+++ src/main/java/github/jcbsm/bridge/util/Mojang.java
@@ -0,0 +1,71 @@
+package github.jcbsm.bridge.util;
+
+import org.apache.commons.lang.StringUtils;
+import org.jetbrains.annotations.Nullable;
+
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.UUID;
+
+public class Mojang {
+
+    @Nullable
+    public static UUID getUserUUID(String username) {
+        try {
+            HttpURLConnection connection = getGetConnection("https://api.minecraftservices.com/minecraft/profile/lookup/name/" + username);
+
+            int status = connection.getResponseCode();
+
+            Reader streamReader = null;
+
+            if (status > 299) {
+                return null;
+            } else {
+                streamReader = new InputStreamReader(connection.getInputStream());
+            }
+
+            BufferedReader reader = new BufferedReader(streamReader);
+            StringBuilder res = new StringBuilder();
+
+            String line;
+
+            while ((line = reader.readLine()) != null) {
+                res.append(line);
+            };
+
+            reader.close();
+            connection.disconnect();
+
+            return getProperUUID(StringUtils.substringBetween(res.toString(), "{", "}").split(",")[0].split(":")[1].replaceAll("\"", "").trim());
+
+        } catch (IOException e) {
+            return null;
+        }
+    }
+
+    private static HttpURLConnection getGetConnection(String url) throws IOException {
+        URL statusURL = new URL(url);
+
+        HttpURLConnection connection = (HttpURLConnection) statusURL.openConnection();
+        connection.setRequestMethod("GET");
+
+        return connection;
+    }
+
+    @Nullable
+    private static UUID getProperUUID(String uuidString){
+
+        if(uuidString.length() != 32){
+            return null;
+        }
+
+        String first = uuidString.substring(0,8);
+        String second = uuidString.substring(8,12);
+        String third = uuidString.substring(12,16);
+        String fourth = uuidString.substring(16,20);
+        String fifth = uuidString.substring(20);
+
+        return UUID.fromString(first+"-"+second+"-"+third+"-"+fourth+"-"+fifth);
+    }
+}
