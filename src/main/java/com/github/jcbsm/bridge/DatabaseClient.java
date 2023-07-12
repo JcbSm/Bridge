6672fa2f4778faf2345daa2369b71636e254aa77
sanamorii <sanamorii@outlook.com>
2023-07-12 21:49:57 +0100

    DatabaseClient: implement account linking

--- src/main/java/com/github/jcbsm/bridge/DatabaseClient.java
+++ src/main/java/com/github/jcbsm/bridge/DatabaseClient.java
@@ -6,6 +6,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.UUID;
 
 public class DatabaseClient {
@@ -67,8 +70,7 @@ public class DatabaseClient {
 
 
     /**
-     * Add corresponding member to the guild's database.
-     * To be used with GuildMemberJoinEvent event
+     * Add corresponding member to the database.
      * @param user JDA user object
      * @throws SQLException where connection/writing to the database results in some error.
      */
@@ -88,36 +90,17 @@ public class DatabaseClient {
         }
     }
 
-
-    public void linkMember(User user, String uuid) throws SQLException{
-        String sql = "INSERT INTO minecraft (`minecraft_uuid`, `minecraft_name`, `member_id`) VALUES (?, ?, ?);" +
-                     "UPDATE members SET member_linked = 1 WHERE member_id = ?;";
-
-        // Add user to database if not already.
-        if (!(this.memberExists(user.getIdLong()))){ this.addMember(user); }
-
-        try (Connection conn = this.connect();
-             PreparedStatement stmt = conn.prepareStatement(sql)){
-            stmt.setString(1, uuid);
-            stmt.setInt(2, 1);  // register as linked
-            stmt.setLong(3, user.getIdLong());
-            stmt.setLong(4, user.getIdLong());
-
-            stmt.executeUpdate();
-        }
-
-        catch (SQLException e){
-            this.logger.warn("Error: " + e.getMessage());
-            throw e;
-        }
-    }
-
-
-    public boolean memberExists(long userID) throws SQLException{
+    /**
+     * Queries the database
+     * @param user JDA user object representing the querying user.
+     * @return true if the user does exist, false otherwise.
+     * @throws SQLException exceptional circumstance such as no connection.
+     */
+    public boolean memberExists(User user) throws SQLException{
         String sql = "SELECT (count(*) > 0) FROM members WHERE member_id = ?";
         try (Connection conn = this.connect();
              PreparedStatement stmt = conn.prepareStatement(sql)){
-            stmt.setLong(1, userID);
+            stmt.setLong(1, user.getIdLong());
 
             ResultSet rs = stmt.executeQuery();
             if (rs.next()){
@@ -132,6 +115,109 @@ public class DatabaseClient {
         }
     }
 
+
+    public int linkAccount(User user, String uuid, String mcName) throws Exception{
+        String insSql = "INSERT INTO minecraft (`minecraft_uuid`, `minecraft_name`, `member_id`) VALUES (?, ?, ?)";
+        String updSql = "UPDATE members SET member_linked = 1 WHERE member_id = ?";
+
+        // Add user to database if not already.
+        if (!(this.memberExists(user))){ this.addMember(user); }
+
+        // Check if the account is already linked with current user or someone else.
+        if (this.isLinked(uuid)){ return 1; }
+
+        try (Connection conn = this.connect()){
+
+            // insert minecraft data
+            PreparedStatement stmt = conn.prepareStatement(insSql);
+            stmt.setString(1, uuid);
+            stmt.setString(2, mcName);  // register as linked
+            stmt.setLong(3, user.getIdLong());
+
+            stmt.executeUpdate();
+            stmt.close();
+
+            // update linked data
+            stmt = conn.prepareStatement(updSql);  // reuse stmt
+            stmt.setLong(1, user.getIdLong());
+
+            stmt.executeUpdate();
+
+            return 0;
+        }
+
+        catch (SQLException e){
+            this.logger.warn("Error: " + e.getMessage());
+            throw e;
+        }
+    }
+
+
+    public void unlinkAccount(String uuid) throws Exception{
+        String sql = "DELETE FROM minecraft WHERE minecraft_uuid = ?";
+
+        try(Connection conn = this.connect();
+            PreparedStatement stmt = conn.prepareStatement(sql)){
+            stmt.setString(1, uuid);
+
+            int res = stmt.executeUpdate();
+        }
+        catch (SQLException e){
+            this.logger.warn("Error: " + e.getMessage());
+            throw e;
+        }
+    }
+
+    public boolean isLinked(String uuid) throws Exception{
+        String sql = "SELECT count(minecraft_uuid) FROM minecraft WHERE minecraft_uuid = ?";
+
+        try(Connection conn = this.connect()){
+            PreparedStatement stmt = conn.prepareStatement(sql);
+            stmt.setString(1, uuid);
+
+            ResultSet rs = stmt.executeQuery();
+
+            if (rs.next()){
+                System.out.println(rs.getInt(1) > 0);
+                System.out.println(rs.getInt(1));
+                return rs.getInt(1) > 0;
+            }
+            else{
+                throw new Exception("Uhhhh");
+            }
+        }
+
+        catch (SQLException e){
+            throw e;
+        }
+    }
+
+
+    public ArrayList<Map<String, String>> getLinkedAccounts(User user) throws SQLException{
+
+        ArrayList<Map<String,String>> accounts = new ArrayList<>();
+        String sql = "SELECT * FROM minecraft WHERE member_id = ?";
+
+        try(Connection conn = this.connect()){
+            PreparedStatement stmt = conn.prepareStatement(sql);
+            stmt.setLong(1, user.getIdLong());
+            ResultSet rs = stmt.executeQuery();
+
+            while (rs.next()){
+                Map<String, String> entry = new HashMap<String, String>();
+                entry.put("username", rs.getString("minecraft_uuid"));
+                entry.put("uuid", rs.getString("minecraft_name"));
+                accounts.add(entry);
+            }
+
+            return accounts;
+        }
+
+        catch (SQLException e){
+            throw e;
+        }
+
+    }
 
     /**
      * Returns the Minecraft UUID currently associated with the provided Discord user ID.
